dc9a410cd36292943ca452c9aa002ff9d7c13fcc
ying <to.ying.wang@outlook.com>
2022-12-07 13:16:13 +0800

    add files

---
+++ external/mLib/include/mLibFreeImage.h
@@ -0,0 +1,12 @@
+//
+// external FreeImage headers
+//
+//#include "FreeImage.h"
+//#include "FreeImage/FreeImage.h"
+#include "/usr/include/FreeImage.h"
+//#include <wchar.h>
+
+//
+// ext-FreeImage headers
+//
+#include "ext-freeimage/freeImageWrapper.h"
